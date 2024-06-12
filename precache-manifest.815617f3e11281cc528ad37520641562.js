c0e9586754aa5110573fc6f645d15bf179d517e1
MattAkou <matthiassery@gmail.com>
2024-06-12 23:46:07 +0800

    Updates

---
+++ precache-manifest.815617f3e11281cc528ad37520641562.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6112b651b409688eebae1ab74d739c05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b9fa0355ff5c84151ba",
+    "url": "/static/css/2.8ac840ac.chunk.css"
+  },
+  {
+    "revision": "4b9fa0355ff5c84151ba",
+    "url": "/static/js/2.b1f1042e.chunk.js"
+  },
+  {
+    "revision": "8fe77b21e4c6bbdaf1ee",
+    "url": "/static/js/main.9f67e8ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ff61d2860bde30a53cf695af9664ddd7",
+    "url": "/static/media/GH.ff61d286.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/defaultNavLogo.06e73328.svg"
+  },
+  {
+    "revision": "ff33b3892fe3333df15da0611603df9c",
+    "url": "/static/media/hero-dark.ff33b389.jpg"
+  },
+  {
+    "revision": "63098c9a46eae3c0561f723ac076d5e0",
+    "url": "/static/media/hero-light.63098c9a.jpg"
+  },
+  {
+    "revision": "9910a56d7fc193cf0620530423ed2d4f",
+    "url": "/static/media/logo.9910a56d.png"
+  },
+  {
+    "revision": "cd80330cbdf297f37c22b5bb7728d2d1",
+    "url": "/static/media/logo.cd80330c.svg"
+  }
+]);
